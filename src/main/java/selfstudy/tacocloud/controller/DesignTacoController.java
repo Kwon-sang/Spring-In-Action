f6a38b5fbe76811cf04578d6be08b6461b53e2ee
Kwon-sang <kwonsg514@gmail.com>
2022-12-16 08:12:39 +0900

    feat(Validation)

--- src/main/java/selfstudy/tacocloud/controller/DesignTacoController.java
+++ src/main/java/selfstudy/tacocloud/controller/DesignTacoController.java
@@ -1,8 +1,10 @@
 package selfstudy.tacocloud.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,7 +54,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         /* 여기서 타코디자인(선택된 식자재 내역)을 저장 */
         log.info("[log] Process Design : {}", design);
         return "redirect:/orders/current";
