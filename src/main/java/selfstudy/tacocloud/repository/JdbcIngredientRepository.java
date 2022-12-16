ee16e422de0a8fd3418f7f8f0a210d8911bcb614
Kwon-sang <kwonsg514@gmail.com>
2022-12-17 05:36:09 +0900

    feat(implement - JdbcIngredientRepository)

--- src/main/java/selfstudy/tacocloud/repository/JdbcIngredientRepository.java
+++ src/main/java/selfstudy/tacocloud/repository/JdbcIngredientRepository.java
@@ -5,6 +5,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import selfstudy.tacocloud.domain.Ingredient;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
 
@@ -17,16 +20,33 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbc.query(
+                "select id, name, type from Ingredient",
+                this::mapRowToIngredient);
     }
 
     @Override
     public Ingredient findById(String id) {
-        return null;
+        return jdbc.queryForObject(
+                "select id, name, type from Ingredient where id = ?",
+                this::mapRowToIngredient,
+                id);
     }
 
     @Override
     public Ingredient save(Ingredient ingredient) {
-        return null;
+        jdbc.update(
+                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString());
+        return ingredient;
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet rs, int rowNum) throws SQLException {
+        return new Ingredient(
+                rs.getString("id"),
+                rs.getString("name"),
+                Ingredient.Type.valueOf(rs.getString("type")));
     }
 }
