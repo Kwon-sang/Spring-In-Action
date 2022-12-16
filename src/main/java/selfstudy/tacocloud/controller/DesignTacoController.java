65075cc429f0de10a5a640dc06ede1c5b0e45e7c
Kwon-sang <kwonsg514@gmail.com>
2022-12-17 08:06:57 +0900

    refactor(connect JdbcIngredientRepository)

--- src/main/java/selfstudy/tacocloud/controller/DesignTacoController.java
+++ src/main/java/selfstudy/tacocloud/controller/DesignTacoController.java
@@ -14,7 +14,6 @@ import selfstudy.tacocloud.domain.Taco;
 import selfstudy.tacocloud.repository.IngredientRepository;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
